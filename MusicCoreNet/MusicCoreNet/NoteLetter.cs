89eb2326dc0f79bc757e86a4ed65902031c8f222
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-08-14 01:10:20 -0400

    Added some basic enums for describing note letters and pitches

---
+++ MusicCoreNet/MusicCoreNet/NoteLetter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Music;
+
+using static NoteLetter;
+
+/// <summary>
+/// Static functionality for the <see cref="NoteLetter"/> enum.
+/// </summary>
+public static class NoteLetters
+{
+    /// <summary>
+    /// Gets the <see cref="NotePitchClass"/> equivalent to the current <see cref="NoteLetter"/> instance.
+    /// </summary>
+    /// <param name="letter"></param>
+    /// <returns></returns>
+    public static NotePitchClass ToPitchClass(this NoteLetter letter) => letter switch
+    {
+        A => NotePitchClass.A,
+        B => NotePitchClass.B,
+        C => NotePitchClass.C,
+        D => NotePitchClass.D,
+        E => NotePitchClass.E,
+        F => NotePitchClass.F,
+        G => NotePitchClass.G,
+        _ => throw new InvalidEnumArgumentException($"Undefined {nameof(NoteLetter)} value."),
+    };
+}
+
+/// <summary>
+/// Represents the classification of a musical note using a letter of the alphabet, from A to G.
+/// </summary>
+public enum NoteLetter
+{
+    /// <summary>
+    /// Represents an 'A' note.
+    /// </summary>
+    A,
+
+    /// <summary>
+    /// Represents a 'B' note.
+    /// </summary>
+    B,
+
+    /// <summary>
+    /// Represents a 'C' note.
+    /// </summary>
+    C,
+
+    /// <summary>
+    /// Represents a 'D' note.
+    /// </summary>
+    D,
+
+    /// <summary>
+    /// Represents an 'E' note.
+    /// </summary>
+    E,
+
+    /// <summary>
+    /// Represents an 'F' note.
+    /// </summary>
+    F,
+
+    /// <summary>
+    /// Represents a 'G' note.
+    /// </summary>
+    G,
+}
+
---
+++ MusicCoreNet/MusicCoreNet/NotePitchClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Music;
+
+/// <summary>
+/// Represents the pitch class of a note, up to octave difference.
+/// </summary>
+public enum NotePitchClass
+{
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to an 'A' note.
+    /// </summary>
+    A,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to an 'A#' or 'Bb' note.
+    /// </summary>
+    AB,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to a 'B' note.
+    /// </summary>
+    B,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to a 'C' note.
+    /// </summary>
+    C,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to a 'C#' or 'Db' note.
+    /// </summary>
+    CD,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to a 'D' note.
+    /// </summary>
+    D,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to a 'D#' or 'Eb' note.
+    /// </summary>
+    DE,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to an 'E' note.
+    /// </summary>
+    E,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to an 'F' note.
+    /// </summary>
+    F,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to a 'F#' or 'Gb' note.
+    /// </summary>
+    FG,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to a 'G' note.
+    /// </summary>
+    G,
+
+    /// <summary>
+    /// Represents notes that are enharmonically equivalent to a 'G#' or 'Ab' note.
+    /// </summary>
+    GA,
+}
