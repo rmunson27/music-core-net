7a4802a767630f985fa88aa443c8369d21c459eb
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-08-14 16:08:59 -0400

    Added interval quality types and testing

---
+++ MusicCoreNet/MusicCoreNet/IntervalQuality.cs
@@ -0,0 +1,243 @@
+﻿using Rem.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Music;
+
+/// <summary>
+/// Represents the quality of a perfectable interval.
+/// </summary>
+public readonly record struct PerfectableIntervalQuality
+{
+    #region Constants
+    /// <summary>
+    /// A perfectable interval quality representing a perfect interval.
+    /// </summary>
+    public static readonly PerfectableIntervalQuality Perfect = new(0);
+    #endregion
+
+    #region Properties And Fields
+    /// <summary>
+    /// Internally represents the value of the quality.
+    /// </summary>
+    private readonly int _numericalValue;
+    #endregion
+
+    #region Constructor
+    private PerfectableIntervalQuality(int NumericalValue) { _numericalValue = NumericalValue; }
+    #endregion
+
+    #region Methods
+    #region Factory
+    /// <summary>
+    /// Creates a new <see cref="PerfectableIntervalQuality"/> representing an augmented interval with the
+    /// given degree.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="Degree"/> was not positive.
+    /// </exception>
+    public static PerfectableIntervalQuality Augmented([Positive] int Degree = 1)
+        => new(Throw.IfArgNotPositive(Degree, nameof(Degree)));
+
+    /// <summary>
+    /// Creates a new <see cref="PerfectableIntervalQuality"/> representing a diminished interval with the
+    /// given degree.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="Degree"/> was not positive.
+    /// </exception>
+    public static PerfectableIntervalQuality Diminished([Positive] int Degree = 1)
+        => new(-Throw.IfArgNotPositive(Degree, nameof(Degree)));
+    #endregion
+
+    #region Classification
+    /// <summary>
+    /// Gets whether or not this interval quality represents an augmented interval, setting the
+    /// <paramref name="Degree"/> parameter to the degree to which it is if so.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    public bool IsAugmented([NonNegative] out int Degree)
+    {
+        if (IsAugmented())
+        {
+            Degree = _numericalValue;
+            return true;
+        }
+        else
+        {
+            Degree = default;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Gets whether or not this interval quality represents an augmented interval.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsAugmented() => _numericalValue > 0;
+
+    /// <summary>
+    /// Gets whether or not this interval quality represents a perfect interval.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsPerfect() => _numericalValue == 0;
+
+    /// <summary>
+    /// Gets whether or not this interval quality represents a diminished interval, setting the
+    /// <paramref name="Degree"/> parameter to the degree to which it is if so.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    public bool IsDiminished([NonNegative] out int Degree)
+    {
+        if (IsDiminished())
+        {
+            Degree = -_numericalValue;
+            return true;
+        }
+        else
+        {
+            Degree = default;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Gets whether or not this interval quality represents a diminished interval.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsDiminished() => _numericalValue < 0;
+    #endregion
+    #endregion
+}
+
+/// <summary>
+/// Represents the quality of a non-perfectable interval.
+/// </summary>
+public readonly record struct NonPerfectableIntervalQuality
+{
+    #region Constants
+    /// <summary>
+    /// A non-perfectable interval quality representing a minor interval.
+    /// </summary>
+    public static readonly NonPerfectableIntervalQuality Minor = new(-1);
+
+    /// <summary>
+    /// A non-perfectable interval quality representing a major interval.
+    /// </summary>
+    public static readonly NonPerfectableIntervalQuality Major = new(0);
+    #endregion
+
+    #region Properties And Fields
+    /// <summary>
+    /// Internally represents the value of the quality.
+    /// </summary>
+    private readonly int _numericalValue;
+    #endregion
+
+    #region Constructor
+    private NonPerfectableIntervalQuality(int NumericalValue) { _numericalValue = NumericalValue; }
+    #endregion
+
+    #region Methods
+    #region Factory
+    /// <summary>
+    /// Creates a new <see cref="NonPerfectableIntervalQuality"/> representing an augmented interval with the
+    /// given degree.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="Degree"/> was not positive.
+    /// </exception>
+    public static NonPerfectableIntervalQuality Augmented([Positive] int Degree = 1)
+        => new(Throw.IfArgNotPositive(Degree, nameof(Degree)));
+
+    /// <summary>
+    /// Creates a new <see cref="NonPerfectableIntervalQuality"/> representing a diminished interval with the
+    /// given degree.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="Degree"/> was not positive.
+    /// </exception>
+    public static NonPerfectableIntervalQuality Diminished([Positive] int Degree = 1)
+        => new(-Throw.IfArgNotPositive(Degree, nameof(Degree)) - 1);
+    #endregion
+
+    #region Classification
+    /// <summary>
+    /// Gets whether or not this interval quality represents an augmented interval, setting the
+    /// <paramref name="Degree"/> parameter to the degree to which it is if so.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    public bool IsAugmented([NonNegative] out int Degree)
+    {
+        if (IsAugmented())
+        {
+            Degree = _numericalValue;
+            return true;
+        }
+        else
+        {
+            Degree = default;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Gets whether or not this interval quality represents an augmented interval.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsAugmented() => _numericalValue > 0;
+
+    /// <summary>
+    /// Gets whether or not this interval quality represents a major interval.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsMajor() => _numericalValue == 0;
+
+    /// <summary>
+    /// Gets whether or not this interval quality represents a minor interval.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsMinor() => _numericalValue == -1;
+
+    /// <summary>
+    /// Gets whether or not this interval quality represents a diminished interval, setting the
+    /// <paramref name="Degree"/> parameter to the degree to which it is if so.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    public bool IsDiminished([NonNegative] out int Degree)
+    {
+        if (IsDiminished())
+        {
+            Degree = -_numericalValue - 1;
+            return true;
+        }
+        else
+        {
+            Degree = default;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Gets whether or not this interval quality represents a diminished interval.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsDiminished() => _numericalValue < 0;
+    #endregion
+    #endregion
+}
---
+++ MusicCoreNet/MusicCoreNetTest/IntervalQualityTest.cs
@@ -0,0 +1,94 @@
+﻿using Rem.Music;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemTest.Music;
+
+/// <summary>
+/// Tests of the <see cref="PerfectableIntervalQuality"/> and <see cref="NonPerfectableIntervalQuality"/> structs.
+/// </summary>
+[TestClass]
+public class IntervalQualityTest
+{
+    #region Properties And Fields
+    private static readonly Random Random = new();
+    #endregion
+
+    #region Perfectable
+    /// <summary>
+    /// Tests the factory method construction and characterization of
+    /// <see cref="PerfectableIntervalQuality"/> instances.
+    /// </summary>
+    [TestMethod, TestCategory(nameof(PerfectableIntervalQuality))]
+    public void TestPerfectableCharacterization()
+    {
+        var randomDegree = Random.Next(10) + 2;
+
+        var randomDiminished = PerfectableIntervalQuality.Diminished(randomDegree);
+        var singleDiminished = PerfectableIntervalQuality.Diminished();
+        var singleAugmented = PerfectableIntervalQuality.Augmented();
+        var randomAugmented = PerfectableIntervalQuality.Augmented(randomDegree);
+
+        int diminishedDegree, augmentedDegree;
+
+        Assert.IsTrue(singleDiminished.IsDiminished());
+        Assert.IsTrue(singleDiminished.IsDiminished(out diminishedDegree));
+        Assert.AreEqual(1, diminishedDegree);
+
+        Assert.IsTrue(randomDiminished.IsDiminished());
+        Assert.IsTrue(randomDiminished.IsDiminished(out diminishedDegree));
+        Assert.AreEqual(randomDegree, diminishedDegree);
+
+        Assert.IsTrue(PerfectableIntervalQuality.Perfect.IsPerfect());
+
+        Assert.IsTrue(singleAugmented.IsAugmented());
+        Assert.IsTrue(singleAugmented.IsAugmented(out augmentedDegree));
+        Assert.AreEqual(1, augmentedDegree);
+
+        Assert.IsTrue(randomAugmented.IsAugmented());
+        Assert.IsTrue(randomAugmented.IsAugmented(out augmentedDegree));
+        Assert.AreEqual(randomDegree, augmentedDegree);
+    }
+    #endregion
+
+    #region Non-Perfectable
+    /// <summary>
+    /// Tests the factory method construction and characterization of
+    /// <see cref="NonPerfectableIntervalQuality"/> instances.
+    /// </summary>
+    [TestMethod, TestCategory(nameof(NonPerfectableIntervalQuality))]
+    public void TestNonPerfectableCharacterization()
+    {
+        var randomDegree = Random.Next(10) + 2;
+
+        var randomDiminished = NonPerfectableIntervalQuality.Diminished(randomDegree);
+        var singleDiminished = NonPerfectableIntervalQuality.Diminished();
+        var singleAugmented = NonPerfectableIntervalQuality.Augmented();
+        var randomAugmented = NonPerfectableIntervalQuality.Augmented(randomDegree);
+
+        int diminishedDegree, augmentedDegree;
+
+        Assert.IsTrue(singleDiminished.IsDiminished());
+        Assert.IsTrue(singleDiminished.IsDiminished(out diminishedDegree));
+        Assert.AreEqual(1, diminishedDegree);
+
+        Assert.IsTrue(randomDiminished.IsDiminished());
+        Assert.IsTrue(randomDiminished.IsDiminished(out diminishedDegree));
+        Assert.AreEqual(randomDegree, diminishedDegree);
+
+        Assert.IsTrue(NonPerfectableIntervalQuality.Minor.IsMinor());
+        Assert.IsTrue(NonPerfectableIntervalQuality.Major.IsMajor());
+
+        Assert.IsTrue(singleAugmented.IsAugmented());
+        Assert.IsTrue(singleAugmented.IsAugmented(out augmentedDegree));
+        Assert.AreEqual(1, augmentedDegree);
+
+        Assert.IsTrue(randomAugmented.IsAugmented());
+        Assert.IsTrue(randomAugmented.IsAugmented(out augmentedDegree));
+        Assert.AreEqual(randomDegree, augmentedDegree);
+    }
+    #endregion
+}
