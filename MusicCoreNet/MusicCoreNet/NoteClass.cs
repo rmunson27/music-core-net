894c6289b185351a6ce692035b2e0a24dedc24c8
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-08-14 01:57:07 -0400

    Added basic `NoteClass` structure

---
+++ MusicCoreNet/MusicCoreNet/NoteClass.cs
@@ -0,0 +1,31 @@
+﻿using Rem.Core.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Music;
+
+/// <summary>
+/// Represents the class of a written note, independent of octave.
+/// </summary>
+/// <param name="Letter">The letter of the note class to create.</param>
+/// <param name="Accidental">The accidental of the note class to create.</param>
+/// <exception cref="InvalidEnumArgumentException"><paramref name="Letter"/> was an unnamed enum value.</exception>
+public readonly record struct NoteClass(NoteLetter Letter, int Accidental)
+{
+    /// <summary>
+    /// Gets or initializes the letter of this note class.
+    /// </summary>
+    /// <exception cref="InvalidEnumPropertySetException">
+    /// This property was initialized to an unnamed enum value.
+    /// </exception>
+    public NoteLetter Letter
+    {
+        get => _letter;
+        init => _letter = Throw.IfEnumPropSetUnnamed(value);
+    }
+    private readonly NoteLetter _letter = Throw.IfEnumArgUnnamed(Letter, nameof(Letter));
+}
