705433cd99821f969db6aed1a650bbd1d001f165
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-08-16 17:16:16 -0400

    Added testing for simple interval base inversion

---
+++ MusicCoreNet/MusicCoreNetTest/SimpleIntervalBaseTest.cs
@@ -0,0 +1,33 @@
+﻿using Rem.Music;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemTest.Music;
+
+/// <summary>
+/// Tests of the <see cref="SimpleIntervalBase"/> class and subclasses.
+/// </summary>
+[TestClass]
+public class SimpleIntervalBaseTest
+{
+    /// <summary>
+    /// Tests negation of a simple interval base.
+    /// </summary>
+    [TestMethod]
+    public void TestNegation()
+    {
+        TestInversionPair(Intervals.Perfect().Fourth(), Intervals.Perfect().Fifth());
+        TestInversionPair(Intervals.Augmented(3).Second(), Intervals.Diminished(3).Seventh());
+        TestInversionPair(Intervals.Minor().Sixth(), Intervals.Major().Third());
+        TestInversionPair(Intervals.Augmented().Fourth(), Intervals.Diminished().Fifth());
+    }
+
+    private static void TestInversionPair(SimpleIntervalBase first, SimpleIntervalBase second)
+    {
+        Assert.AreEqual(first, -second);
+        Assert.AreEqual(second, -first);
+    }
+}
