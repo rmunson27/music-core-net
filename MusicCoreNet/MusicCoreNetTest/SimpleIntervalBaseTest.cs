a26daec8ceca1fe740a49cc99694ca860ce7999d
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-08-16 17:45:23 -0400

    Added testing for `SimpleIntervalBase` addition
    The two extant test methods are sufficient to test subtraction as well, as subtraction is defined as a combination of the two methods under test.

--- MusicCoreNet/MusicCoreNetTest/SimpleIntervalBaseTest.cs
+++ MusicCoreNet/MusicCoreNetTest/SimpleIntervalBaseTest.cs
@@ -14,7 +14,7 @@ namespace RemTest.Music;
 public class SimpleIntervalBaseTest
 {
     /// <summary>
-    /// Tests negation of a simple interval base.
+    /// Tests negation of <see cref="SimpleIntervalBase"/> instances.
     /// </summary>
     [TestMethod]
     public void TestNegation()
@@ -29,5 +29,25 @@ public class SimpleIntervalBaseTest
     {
         Assert.AreEqual(first, -second);
         Assert.AreEqual(second, -first);
+    }
+
+    /// <summary>
+    /// Tests addition of <see cref="SimpleIntervalBase"/> instances.
+    /// </summary>
+    [TestMethod]
+    public void TestAddition()
+    {
+        TestAdditionPair(Intervals.Major().Second(), Intervals.Major().Second(), Intervals.Major().Third());
+        TestAdditionPair(Intervals.Perfect().Fourth(), Intervals.Perfect().Fourth(), Intervals.Minor().Seventh());
+        TestAdditionPair(Intervals.Augmented(2).Third(), Intervals.Diminished(2).Sixth(), Intervals.Perfect().Unison());
+        TestAdditionPair(Intervals.Major().Sixth(), Intervals.Minor().Second(), Intervals.Minor().Seventh());
+        TestAdditionPair(Intervals.Major().Seventh(), Intervals.Major().Sixth(), Intervals.Augmented().Fifth());
+    }
+
+    private static void TestAdditionPair(
+        SimpleIntervalBase first, SimpleIntervalBase second, SimpleIntervalBase expectedResult)
+    {
+        Assert.AreEqual(expectedResult, first + second);
+        Assert.AreEqual(expectedResult, second + first);
     }
 }
