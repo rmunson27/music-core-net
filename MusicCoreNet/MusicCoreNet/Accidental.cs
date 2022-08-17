84c70cf2018806419ee6327383a87929277dd024
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-08-17 18:16:40 -0400

    Added a struct representing accidentals

---
+++ MusicCoreNet/MusicCoreNet/Accidental.cs
@@ -0,0 +1,181 @@
+﻿using Rem.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Music;
+
+/// <summary>
+/// Represents an accidental of a musical note.
+/// </summary>
+/// <remarks>
+/// The default instance of this struct represents a natural.
+/// </remarks>
+public readonly record struct Accidental
+{
+    #region Constants
+    /// <summary>
+    /// An accidental representing a natural.
+    /// </summary>
+    public static readonly Accidental Natural = new(0);
+    #endregion
+
+    #region Properties
+    /// <summary>
+    /// Gets an <see cref="int"/> value describing the current instance.
+    /// </summary>
+    /// <remarks>
+    /// The value of this property will be positive if the current instance is sharp, zero if it is natural, and
+    /// negative if it is flat.
+    /// </remarks>
+    public int AsInt => _intValue;
+
+    /// <summary>
+    /// Internally represents the value of the accidental.
+    /// </summary>
+    private readonly int _intValue;
+    #endregion
+
+    #region Constructor
+    /// <summary>
+    /// Constructs a new instance of this struct.
+    /// </summary>
+    /// <param name="IntValue"></param>
+    private Accidental(int IntValue) { this._intValue = IntValue; }
+    #endregion
+
+    #region Methods
+    #region Factory
+    /// <summary>
+    /// Creates a new <see cref="Accidental"/> representing a sharp with the given integer degree.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="Degree"/> was not positive.</exception>
+    public static Accidental Sharp([Positive] int Degree = 1) => new(Throw.IfArgNotPositive(Degree, nameof(Degree)));
+
+    /// <summary>
+    /// Creates a new <see cref="Accidental"/> representing a flat with the given integer degree.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="Degree"/> was not positive.</exception>
+    public static Accidental Flat([Positive] int Degree = 1) => new(-Throw.IfArgNotPositive(Degree, nameof(Degree)));
+    #endregion
+
+    #region Classification
+    /// <summary>
+    /// Gets whether or not the current <see cref="Accidental"/> is sharp.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsSharp() => _intValue > 0;
+
+    /// <summary>
+    /// Gets whether or not the current <see cref="Accidental"/> is sharp, returning the degree to which it is in an
+    /// <see langword="out"/> parameter if so.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    public bool IsSharp([NonNegative] out int Degree)
+    {
+        if (_intValue > 0)
+        {
+            Degree = _intValue;
+            return true;
+        }
+        else
+        {
+            Degree = default;
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Gets whether or not the current <see cref="Accidental"/> is natural.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsNatural() => _intValue == 0;
+
+    /// <summary>
+    /// Gets whether or not the current <see cref="Accidental"/> is flat.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsFlat() => _intValue < 0;
+
+    /// <summary>
+    /// Gets whether or not the current <see cref="Accidental"/> is flat, returning the degree to which it is in an
+    /// <see langword="out"/> parameter if so.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    public bool IsFlat([NonNegative] out int Degree)
+    {
+        if (_intValue < 0)
+        {
+            Degree = -_intValue;
+            return true;
+        }
+        else
+        {
+            Degree = default;
+            return false;
+        }
+    }
+    #endregion
+
+    #region Modifiers
+    /// <summary>
+    /// Gets an <see cref="Accidental"/> equivalent to the current instance sharpened by the given degree.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    public Accidental Sharpen([NonNegative] int Degree = 1)
+        => new(_intValue + Throw.IfArgNegative(Degree, nameof(Degree)));
+
+    /// <summary>
+    /// Gets an <see cref="Accidental"/> equivalent to the current instance flattened by the given degree.
+    /// </summary>
+    /// <param name="Degree"></param>
+    /// <returns></returns>
+    public Accidental Flatten([NonNegative] int Degree = 1)
+        => new(_intValue - Throw.IfArgNegative(Degree, nameof(Degree)));
+
+    /// <summary>
+    /// Gets an <see cref="Accidental"/> equivalent to the current instance sharpened or flattened by a degree
+    /// determined by the magnitude and sign of the integer passed in.
+    /// </summary>
+    /// <remarks>
+    /// The returned value will be sharper than the current instance if <paramref name="Amount"/> is positive,
+    /// and flatter if <paramref name="Amount"/> is negative.
+    /// </remarks>
+    /// <param name="Amount"></param>
+    /// <returns></returns>
+    public Accidental Shift(int Amount) => new(_intValue + Amount);
+    #endregion
+
+    #region ToString
+    /// <summary>
+    /// Gets a <see cref="string"/> that represents the current <see cref="Accidental"/>.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => _intValue switch
+    {
+        < 0 => $"Flat {{ Degree = {-_intValue} }}",
+        0 => "Natural",
+        > 0 => $"Sharp {{ Degree = {_intValue} }}",
+    };
+    #endregion
+    #endregion
+}
+
+/// <summary>
+/// Represents the type of an accidental.
+/// </summary>
+public enum AccidentalType : sbyte
+{
+    Flat = -1,
+    Natural = 0,
+    Sharp = 1,
+}
--- MusicCoreNet/MusicCoreNet/NoteClass.cs
+++ MusicCoreNet/MusicCoreNet/NoteClass.cs
@@ -17,7 +17,7 @@ namespace Rem.Music;
 /// <param name="Letter">The letter of the note class to create.</param>
 /// <param name="Accidental">The accidental of the note class to create.</param>
 /// <exception cref="InvalidEnumArgumentException"><paramref name="Letter"/> was an unnamed enum value.</exception>
-public readonly record struct NoteClass(NoteLetter Letter, int Accidental)
+public readonly record struct NoteClass(NoteLetter Letter, Accidental Accidental = default)
 {
     /// <summary>
     /// Gets or initializes the letter of this note class.
@@ -31,4 +31,24 @@ public readonly record struct NoteClass(NoteLetter Letter, int Accidental)
         init => _letter = Throw.IfEnumPropSetUnnamed(value);
     }
     private readonly NoteLetter _letter = Throw.IfEnumArgUnnamed(Letter, nameof(Letter));
+
+    /// <summary>
+    /// Gets the difference between the two <see cref="NoteClass"/> instances passed in as an instance
+    /// of <see cref="SimpleIntervalBase"/>.
+    /// </summary>
+    /// <param name="lhs"></param>
+    /// <param name="rhs"></param>
+    /// <returns></returns>
+    public static SimpleIntervalBase operator -(NoteClass lhs, NoteClass rhs)
+    {
+#pragma warning disable CS8509 // This should handle everything
+        return lhs.Letter.Minus(rhs.Letter) switch
+#pragma warning restore CS8509
+        {
+            PerfectableSimpleIntervalBase pi
+                => pi with { Quality = pi.Quality.Shift(lhs.Accidental.AsInt - rhs.Accidental.AsInt) },
+            NonPerfectableSimpleIntervalBase npi
+                => npi with { Quality = npi.Quality.Shift(lhs.Accidental.AsInt - rhs.Accidental.AsInt) },
+        };
+    }
 }
