3bc0650373950a39f62f8134fcc13e8b6faf526f
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-08-21 03:46:19 -0400

    Replaced the `CircleOfFifths` simple interval base comparer with a note class comparer
    The simple interval base comparison is trivial since there is a `CircleOfFifthsIndex` property on that type, whereas no reasonable analogous property can be exposed for `NoteClass` since it would have to be relative to an arbitrarily selected `NoteClass` constant.

--- MusicCoreNet/MusicCoreNet/CircleOfFifths.cs
+++ MusicCoreNet/MusicCoreNet/CircleOfFifths.cs
@@ -1,42 +1,28 @@
-﻿using Rem.Core.Attributes;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Rem.Music;
 
-using static PerfectableSimpleIntervalNumber;
-using static NonPerfectableSimpleIntervalNumber;
-
 /// <summary>
-/// Helper methods for dealing with the circle of fifths.
+/// Helper functionality relating to the circle of fifths.
 /// </summary>
 public static class CircleOfFifths
 {
     /// <summary>
-    /// A comparer that can be used to compare <see cref="SimpleIntervalBase"/> instances based on their position in
+    /// A comparer that can be used to compare <see cref="NoteClass"/> instances based on their position in
     /// the circle of fifths.
     /// </summary>
-    public static readonly IComparer<SimpleIntervalBase> Comparer = new SimpleIntervalBaseComparer();
+    public static readonly IComparer<NoteClass> NoteClassComparer = new NoteClassComparerType();
 
-    private sealed class SimpleIntervalBaseComparer : IComparer<SimpleIntervalBase>
+    private sealed class NoteClassComparerType : IComparer<NoteClass>
     {
         /// <inheritdoc/>
-        public int Compare(SimpleIntervalBase? x, SimpleIntervalBase? y)
+        public int Compare(NoteClass x, NoteClass y)
         {
-            // Set `null` to compare as below any other value
-            if (x is null) return y is null ? 0 : -1;
-            else if (y is null) return 1;
-
-            // Compare the qualities and then the numbers if the qualities are equal
-            return x.Quality.CircleOfFifthsIndex.CompareTo(y.Quality.CircleOfFifthsIndex) switch
-            {
-                0 => x.Number.CircleOfFifthsIndex().CompareTo(y.Number.CircleOfFifthsIndex()),
-                var comp => comp,
-            };
+            return x.CircleOfFifthsIndexRelativeToC.CompareTo(y.CircleOfFifthsIndexRelativeToC);
         }
     }
 }
