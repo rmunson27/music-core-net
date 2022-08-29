e4dca627dd5262589dfe6360ace044c41fdb0855
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-08-29 15:47:41 -0400

    Fixed a miswritten namespace in "NotePitchClassTest.cs"

--- MusicCoreNet/MusicCoreNetTest/Notes/NotePitchClassTest.cs
+++ MusicCoreNet/MusicCoreNetTest/Notes/NotePitchClassTest.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RemTest.Music.Notes;
+namespace RemTest.Music;
 
 /// <summary>
 /// Tests of the <see cref="NotePitchClass"/> struct.
