2c8e0a099fcc3292272471f60543f04b316ca88c
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-12-25 17:54:47 -0500

    Removed some unused imports

--- MusicCoreNet/MusicCoreNet/Notes/NoteSpelling.cs
+++ MusicCoreNet/MusicCoreNet/Notes/NoteSpelling.cs
@@ -1,8 +1,6 @@
-﻿using Rem.Core.Attributes;
-using Rem.Music.Internal;
+﻿using Rem.Music.Internal;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
